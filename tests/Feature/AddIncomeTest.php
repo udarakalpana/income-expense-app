4f4e75fa583222e28812746a5875a36d62d32161
kalpana udara <kalpanaudara058@gmail.com>
2024-12-14 12:06:59 +0530

    Create first test case

---
+++ tests/Feature/AddIncomeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Tests\TestCase;
+
+final class AddIncomeTest extends TestCase
+{
+    public function test_income_and_category_database_save()
+    {
+        // A - Arrange
+        // dummy data that we need to do the test
+
+        // A - Act / Action
+        // implement that we need to test part. (endpoint/ class/ function)
+
+        // A - Assertion
+        //
+
+    }
+}
