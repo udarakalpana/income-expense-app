08e5c3e02ba133324b8f0d54d744ef8771c7d396
kalpana udara <kalpanaudara058@gmail.com>
2024-12-14 10:46:05 +0530

    Create income and expense dashboard sidebar collapse functiolities

--- resources/frontend/src/components/common/sidebar/sideBarButtons/TransactionManagementButton.jsx
+++ resources/frontend/src/components/common/sidebar/sideBarButtons/TransactionManagementButton.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router";
 
 const TransactionManagementButton = () => {
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false)
+
+    const handleDropDownOpenAndClose = (isOpen) => {
+        if (!isOpen) {
+            setIsDropDownOpen(true)
+            return
+        }
+
+        setIsDropDownOpen(false)
+    }
+
     return (
         <li>
-            <a href="#"
+            <Link
+                to="/add-income"
+                onClick={() => handleDropDownOpenAndClose(isDropDownOpen)}
                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <svg
                     className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -14,9 +27,9 @@ const TransactionManagementButton = () => {
                         d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Transaction Management</span>
-            </a>
+            </Link>
 
-            <ul className='py-2 space-y-4'>
+            <ul className={isDropDownOpen ? 'py-2 space-y-4' : 'hidden'}>
                 <li className='ml-12'>
                     <Link to='/add-income'>Add Income</Link>
                 </li>
--- resources/frontend/src/components/dashboard/ dashboardComponents/transactionManagement/AddExpense.jsx
+++ resources/frontend/src/components/dashboard/ dashboardComponents/transactionManagement/AddExpense.jsx
@@ -3,69 +3,12 @@ import React from 'react';
 const AddExpense = () => {
     return (
         <div className="p-4 sm:ml-64">
-            <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                <div className="grid grid-cols-3 gap-4 mb-4">
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        Add Expenses Section
-                    </div>
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                             fill="none"
-                             viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                  stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
-                    </p>
-                </div>
+            <div className="p-4">
                 <div className="grid grid-cols-2 gap-4 mb-4">
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
+                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add Expense</h2>
                     </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
                         <p className="text-2xl text-gray-400 dark:text-gray-500">
                             <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                  fill="none" viewBox="0 0 18 18">
@@ -75,53 +18,38 @@ const AddExpense = () => {
                         </p>
                     </div>
                 </div>
-                <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                             fill="none"
-                             viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                  stroke-width="2" d="M9 1v16M1 9h16"/>
-                        </svg>
-                    </p>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
-                    <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M9 1v16M1 9h16"/>
-                            </svg>
-                        </p>
-                    </div>
+                <div className="mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                    <section className="bg-white dark:bg-gray-900">
+                        <div className="py-1 w-3/4 m-auto lg:py-16">
+                            <form action="#">
+                                <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                    <div className="sm:col-span-2">
+                                        <label htmlFor="income_amount"
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Expense
+                                            Amount</label>
+                                        <input type="number" name="income_amount" id="income_amount"
+                                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                               placeholder="Type income amount" required=""/>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="category"
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                                        <select id="category"
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                            <option selected="">Select Expense Type</option>
+                                            <option value="salary">Salary</option>
+                                            <option value="side project">Side Project</option>
+                                            <option value="youtube revenue">YouTube Revenue</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <button type="submit"
+                                        className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                                    Add product
+                                </button>
+                            </form>
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
