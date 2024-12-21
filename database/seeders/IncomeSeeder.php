751c69094ff7eab5886f3fb4fd39f07cdd1e780d
kalpana udara <kalpanaudara058@gmail.com>
2024-12-21 10:09:33 +0530

    Create income model, factory and seeder

---
+++ database/seeders/IncomeSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Income;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class IncomeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Income::factory()->count(100)->create();
+    }
+}
